8d697d584632b5baeab1bf41c509ad750216bb89
Nicholas Boyd Isacsson <nicholas@isacsson.se>
2023-10-25 08:40:24 +0200

    Strip out unused runtime AST parsing to own method

--- src/java/lang/Compiler.java
+++ src/java/lang/Compiler.java
@@ -29,64 +29,18 @@ public class Compiler {
 		try {
 			RuleSet ruleSet = (RuleSet) parse(new FileReader(args[0]));
 
-      Configuration config = new Configuration(new String[]{args[1]}, System.err);
-
-      JastAddChild jac = new JastAddChild(config);
-      Grammar grammar = jac.buildGrammar();
-      for (ASTDecl root: grammar.roots()) {
-        System.out.println(root);
-        for(ASTDecl sub : root.parents()) {
-          System.out.println("  " + sub);
-        }
-      }
-      System.out.println(grammar.subclassMap());
-      System.out.println();
-      for (ASTDecl i : grammar.subclassMap().keySet()) {
-        System.out.println(i);
-        System.out.println("  Subclasses:" + grammar.subclassMap().get(i));
-        for (Component comp : i.getComponents()) {
-          System.out.println("  " + comp);
-          System.out.println("    typeDecl: " + comp.typeDecl());
-          System.out.println("    hostClass: " + comp.hostClass());
-          System.out.println("    name: " + comp.name());
-          System.out.println("    type: " + comp.type());
-          System.out.println("    kind: " + comp.kind());
-        }
-      }
-      System.out.println();
-      System.out.println(grammar.parentMap());
-      System.out.println(grammar.getTypeDeclList());
-
-      String[] builtInNames = {"Opt", "List"};
-      List<ASTDecl> builtIns = new ArrayList<ASTDecl>();
-      for (ASTDecl i : grammar.subclassMap().keySet())
-        for ( String b : builtInNames)
-          if (i.toString().equals(b))
-            builtIns.add(i);
-
-      List<ASTDecl> terminals = new ArrayList<ASTDecl>();
-      for (ASTDecl i : grammar.subclassMap().keySet()) {
-        if (grammar.subclassMap().get(i).isEmpty()) {
-          terminals.add(i);
-        }
-      }
-      //terminals.removeAll(grammar.roots());
-      terminals.removeAll(builtIns);
-
-      for (ASTDecl i : terminals)
-        System.out.println("Terminal: " + i);
+      /*
+       * This was intended to be used for verifying the rule definition against
+       * the AST definition, to provide early feedback on errors, but did not
+       * end up getting finished.
+       */
+      //parseObjectLanguageAST(args[1]);
 
       Path outputDir = Paths.get("out");
       OutputGeneration output = new OutputGeneration(outputDir);
       output.prepareOutputDir();
       output.generateTypeChecker(ruleSet);
       output.copyASTDef(args[1]);
-
-//      for (TypeDecl td: grammar.findSubClasses(grammar.roots().get(0))) {
-//        System.out.println(td);
-//      }
-
-
 
 		} catch (FileNotFoundException e) {
 			System.err.println(e.getMessage());
@@ -108,4 +62,59 @@ public class Compiler {
 		}
     return null;
   }
+
+  private static void parseObjectLanguageAST(String astDef){
+    Configuration config = new Configuration(new String[]{astDef}, System.err);
+
+    JastAddChild jac = new JastAddChild(config);
+    Grammar grammar = jac.buildGrammar();
+    for (ASTDecl root: grammar.roots()) {
+      System.out.println(root);
+      for(ASTDecl sub : root.parents()) {
+        System.out.println("  " + sub);
+      }
+    }
+    System.out.println(grammar.subclassMap());
+    System.out.println();
+    for (ASTDecl i : grammar.subclassMap().keySet()) {
+      System.out.println(i);
+      System.out.println("  Subclasses:" + grammar.subclassMap().get(i));
+      for (Component comp : i.getComponents()) {
+        System.out.println("  " + comp);
+        System.out.println("    typeDecl: " + comp.typeDecl());
+        System.out.println("    hostClass: " + comp.hostClass());
+        System.out.println("    name: " + comp.name());
+        System.out.println("    type: " + comp.type());
+        System.out.println("    kind: " + comp.kind());
+      }
+    }
+    System.out.println();
+    System.out.println(grammar.parentMap());
+    System.out.println(grammar.getTypeDeclList());
+
+    String[] builtInNames = {"Opt", "List"};
+    List<ASTDecl> builtIns = new ArrayList<ASTDecl>();
+    for (ASTDecl i : grammar.subclassMap().keySet())
+      for ( String b : builtInNames)
+        if (i.toString().equals(b))
+          builtIns.add(i);
+
+    List<ASTDecl> terminals = new ArrayList<ASTDecl>();
+    for (ASTDecl i : grammar.subclassMap().keySet()) {
+      if (grammar.subclassMap().get(i).isEmpty()) {
+        terminals.add(i);
+      }
+    }
+    //terminals.removeAll(grammar.roots());
+    terminals.removeAll(builtIns);
+
+    for (ASTDecl i : terminals)
+      System.out.println("Terminal: " + i);
+
+    //for (TypeDecl td: grammar.findSubClasses(grammar.roots().get(0))) {
+    //  System.out.println(td);
+    //}
+
+  }
+
 }
